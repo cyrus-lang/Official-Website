2c1fcb667957858ff3b10ae71983f92d5ce228b0
Taha. Dostifam <mr.tahadostifam@gmail.com>
2025-06-22 23:42:38 +0330

    improve ErrorAlert colors

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -7,10 +7,10 @@ import Link from 'next/link';
 
 export function ErrorAlert({ title, children }: { title?: string, children: React.ReactNode }) {
   return (
-    <div className="bg-red-50 border-s-4 border-red-500 p-4 dark:bg-red-800/30 mb-4" role="alert">
+    <div className="bg-red-50 border-s-4 border-red-500 p-4 dark:bg-red-800/20 mb-4" role="alert">
       <div className="flex">
         <div className="shrink-0">
-          <span className="inline-flex justify-center items-center size-8 rounded-full border-4 border-red-100 bg-red-200 text-red-800 dark:border-red-900 dark:bg-red-800 dark:text-red-400">
+          <span className="inline-flex justify-center items-center size-8 rounded-full border-4 border-red-100 bg-red-200 text-red-800 dark:border-red-950 dark:bg-red-800 dark:text-red-100">
             <svg className="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor">
               <path d="M18 6 6 18"></path>
               <path d="m6 6 12 12"></path>
